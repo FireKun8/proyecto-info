1a9a0164c369e3a146107d77ec5a7a44c6a6ea82
rdomenech12 <rdomenechcalatayud@gmail.com>
2021-12-30 11:51:46 +0100

    Suma2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@ int main(){
     cout << "Hola porc!" << endl;
     cout << "hola";
 
-    int x, y, suma = 0;
+    int x = 7, y = 2, suma = 0;
+
+    suma = x + y;
+    
+    cout << suma << endl;
 
 
+    return 0;
 }
