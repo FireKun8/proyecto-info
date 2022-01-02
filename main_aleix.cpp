bc2ce4caac182d728439537d0a26b943541b0298
FireKun8 <aleixribastorras@gmail.com>
2022-01-02 01:05:00 +0100

    formato

--- main_aleix.cpp
+++ main_aleix.cpp
@@ -11,8 +11,6 @@ const int NCAT = 3;
 
 typedef int mPobl[NANOS][NCAT];
 
-
-
 struct tLocalizaciones
 {
     int codigo;
@@ -28,6 +26,7 @@ struct tDatos
 typedef tDatos mIndex[NLOC];
 typedef tLocalizaciones mGestorLoc[NLOC];
 
+void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones);
 void imprimirDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, int intro);
 
 
@@ -36,6 +35,14 @@ int main(){
 
     mIndex mMigraciones;
     mGestorLoc mLocalizaciones;
+
+    leerDatos(mMigraciones, mLocalizaciones);
+    imprimirDatos(mMigraciones, mLocalizaciones, 1);
+    return 0;
+}
+
+void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones){
+    ifstream datos("migraciones.csv");
 
     string linea;
     int ref, codigo;
@@ -67,28 +74,31 @@ int main(){
             contador++;
         }
     }
-
-    imprimirDatos(mMigraciones, mLocalizaciones, 1);
-    return 0;
 }
 
 void imprimirDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, int intro){
-    cout << setw(45) << left << "TERRITORIO|" << setw(10) << "CODIGO|" << setw(12) << 1990 << "|" << setw(12) << 1995 << "|" << setw(12) << 2000 << "|" << setw(12) << 2005 << "|" << setw(12) << 2010 << "|" << setw(12) << 2015 << "|" << setw(12) << 2020 << "|" << endl;
-    for(int i = 0; i < 139; i++){
-        cout << "-";
+    cout << "| " << setw(50) << left << "TERRITORIO" << "| " << setw(10) << "CODIGO" << "| " << setw(12) << 1990 << "| " << setw(12) << 1995  << "| " << setw(12) << 2000 << "| " << setw(12) << 2005 << "| " << setw(12) << 2010 << "| " << setw(12) << 2015 << "| " << setw(12) << 2020 << endl;
+    for(int i = 0; i < 159; i++){
+        if(i == 0){
+            cout << "| ";
+        }
+        else{
+            cout << "-";
+        }
     }
     cout << endl;
     for(int i = 0; i < NLOC; i++){
-        cout << setw(45) << mLocalizaciones[i].localizacion << setw(10) << mMigraciones[i].lCodigo;
+        //separadores de continentes
+        cout << "| " << setw(50) << mLocalizaciones[i].localizacion << "| " << setw(10) << mMigraciones[i].lCodigo;
         for(int j = 0; j < NANOS; j++){
             if(intro == 1){
-                cout << setw(12) << mMigraciones[i].poblTotal[j][0] << "|";
+                cout << "| " << setw(12) << mMigraciones[i].poblTotal[j][0];
             }
             if(intro == 2){
-                cout << setw(12) << mMigraciones[i].poblTotal[j][1] << "|";
+                cout << "| " << setw(12) << mMigraciones[i].poblTotal[j][1];
             }
             if(intro == 3){
-                cout << setw(12) << mMigraciones[i].poblTotal[j][2] << "|";
+                cout << "| " << setw(12) << mMigraciones[i].poblTotal[j][2];
             }
         }
         cout << endl;
