06ef28a627de39a769abcd4187eb2d641caaf15b
FireKun8 <aleixribastorras@gmail.com>
2022-01-03 00:54:36 +0100

    listador

--- main_aleix.cpp
+++ main_aleix.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
 const int NLOC = 283;
@@ -23,11 +24,19 @@ struct tDatos
     mPobl poblTotal;
 };
 
+struct tListador
+{
+    int codigo;
+    string localizacion;
+    mPobl poblTotal;
+};
+
 typedef tDatos mIndex[NLOC];
 typedef tLocalizaciones mGestorLoc[NLOC];
 
-void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones);
-void imprimirDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, int intro);
+void leerDatos(mIndex& mMigraciones, mGestorLoc& mLocalizaciones, vector<int>& vLocPadre);
+void listadorDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, vector<tListador>& vLista, vector<int> vDemandado);
+void imprimirDatos(vector<tListador>& vLista, int intro);
 
 
 int main(){
@@ -35,13 +44,17 @@ int main(){
 
     mIndex mMigraciones;
     mGestorLoc mLocalizaciones;
+    vector<int> vLocPadre;
+    vector<tListador> vLista;
+    vector<int> vDemandado = {900, 728};
 
-    leerDatos(mMigraciones, mLocalizaciones);
-    imprimirDatos(mMigraciones, mLocalizaciones, 1);
+    leerDatos(mMigraciones, mLocalizaciones, vLocPadre);
+    listadorDatos(mMigraciones, mLocalizaciones, vLista, vDemandado);
+    imprimirDatos(vLista, 1);
     return 0;
 }
 
-void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones){
+void leerDatos(mIndex& mMigraciones, mGestorLoc& mLocalizaciones, vector<int>& vLocPadre){
     ifstream datos("migraciones.csv");
 
     string linea;
@@ -50,6 +63,7 @@ void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones){
     string localizacion;
     int numDatos[3][7];
     int contador = 0;
+    bool temp = false;
 
     if(datos.is_open()){
         while(getline(datos, linea)){
@@ -57,8 +71,14 @@ void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones){
             getline(ss, aux, ',');
             getline(ss, aux, ',');
             mLocalizaciones[contador].localizacion = aux;
+            if(aux[2] <= 65 && aux[2] >= 90){
+                temp = true;
+            }
             getline(ss, aux, ',');
             mMigraciones[contador].lCodigo = stoi(aux);
+            if(temp == true){
+                vLocPadre.push_back(stoi(aux));
+            }
             mLocalizaciones[contador].codigo = stoi(aux);
             for(int i = 0; i < NCAT; i++){
                 for(int j = 0; j < NANOS; j++){
@@ -76,7 +96,28 @@ void leerDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones){
     }
 }
 
-void imprimirDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, int intro){
+void listadorDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, vector<tListador>& vLista, vector<int> vDemandado){
+    for(int i = 0; i < vDemandado.size(); i++){
+        vLista.push_back(tListador());
+        bool encontrado = false;
+        int index = 0;
+        while(!encontrado){
+            if(vDemandado[i] == mLocalizaciones[index].codigo){
+                vLista[i].codigo = mLocalizaciones[index].codigo;
+                vLista[i].localizacion = mLocalizaciones[index].localizacion;
+                encontrado = true;
+            }
+            index++;
+        }
+        for(int j = 0; j < NCAT; j++){
+            for(int k = 0; k < NANOS; k++){
+                vLista[i].poblTotal[k][j] = mMigraciones[i].poblTotal[k][j];
+            }
+        }
+    }
+}
+
+void imprimirDatos(vector<tListador>& vLista, int intro){
     cout << "| " << setw(50) << left << "TERRITORIO" << "| " << setw(10) << "CODIGO" << "| " << setw(12) << 1990 << "| " << setw(12) << 1995  << "| " << setw(12) << 2000 << "| " << setw(12) << 2005 << "| " << setw(12) << 2010 << "| " << setw(12) << 2015 << "| " << setw(12) << 2020 << endl;
     for(int i = 0; i < 159; i++){
         if(i == 0){
@@ -87,20 +128,19 @@ void imprimirDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, int intro){
         }
     }
     cout << endl;
-    for(int i = 0; i < NLOC; i++){
-        //separadores de continentes
-        cout << "| " << setw(50) << mLocalizaciones[i].localizacion << "| " << setw(10) << mMigraciones[i].lCodigo;
+    for(int i = 0; i < vLista.size(); i++){
+        cout << "| " << setw(50) << vLista[i].localizacion << "| " << setw(10) << vLista[i].codigo; 
         for(int j = 0; j < NANOS; j++){
             if(intro == 1){
-                cout << "| " << setw(12) << mMigraciones[i].poblTotal[j][0];
+                cout << "| " << setw(12) << vLista[i].poblTotal[j][0];
             }
             if(intro == 2){
-                cout << "| " << setw(12) << mMigraciones[i].poblTotal[j][1];
+                cout << "| " << setw(12) << vLista[i].poblTotal[j][1];
             }
             if(intro == 3){
-                cout << "| " << setw(12) << mMigraciones[i].poblTotal[j][2];
+                cout << "| " << setw(12) << vLista[i].poblTotal[j][2];
             }
         }
         cout << endl;
-    } 
+    }
 }
