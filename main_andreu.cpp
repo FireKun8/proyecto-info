4adb4e04d449c8496aa2be9594384def05e2f637
andreu-craus <andreu.craus@estudiantat.upc.edu>
2022-01-03 21:03:23 +0100

    Andreu 10

--- main_andreu.cpp
+++ main_andreu.cpp
@@ -18,26 +18,39 @@ typedef struct tDatos
 typedef tDatos mDatos[NP][NP];
 
 
-int asignador(string aux)
+int asignador(string ISO)
 {
     int asign;
     ifstream ISO("asign_ISO.csv");
-    string line;
+    string line, ISO1;
+
     if (asign_ISO.is_open())
     {
-
+        bool trobat = false;
+        while (getline(asign, line, '\n')&& !trobat)
+        {
+            stringstream ss(line);
+            getline (ss, asign, ';');
+            getline (ss, pais, ';');
+            getline (ss, ISO1, ';');
+            if (ISO1==ISO)
+            {
+                trobat=true;
+            }
+            
+        }
+        
     }
-    
+    return asign;
 }
 
-void leerDatos_ref()
+void leerDatos_ref(mDatos& mDatos2016,mDatos& mDatos2017,mDatos& mDatos2018,mDatos& mDatos2019,mDatos& mDatos2020,mDatos& mDatos2021;)
 {
     ifstream datos("refugiados.csv");
     string line;
-    string any, aux;
-    int i,j;
-    mDatos mDatos2016, mDatos2017, mDatos2018, mDatos2019, mDatos2020, mDatos2021;
-    
+    string aux;
+    int i, j, any;
+        
     if (datos.is_open())
     {
         while (getline(datos, line,'\n')) 
@@ -95,13 +108,10 @@ void leerDatos_ref()
                     getline(ss, mDatos2021[i][j].IDP, ',');
                     break;
                 }
-                
+               
             }
-            
-            
-          
-        }
-        
+                        
+        }        
 
     
     }
