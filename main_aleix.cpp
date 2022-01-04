4c939030335b8301e8809c58a9520ed387dc004e
FireKun8 <aleixribastorras@gmail.com>
2022-01-04 22:32:37 +0100

    actualizacion

--- main_aleix.cpp
+++ main_aleix.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <iomanip>
 #include <vector>
+#include <conio.h>
 using namespace std;
 
 const int NLOC = 283;
@@ -38,6 +39,7 @@ void leerDatos(mIndex& mMigraciones, mGestorLoc& mLocalizaciones, vector<int>& v
 void listadorDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, vector<tListador>& vLista, vector<int> vDemandado);
 void imprimirDatos(vector<tListador>& vLista, int intro);
 void menu(vector<tListador> vLista, vector<int> vDemandado, mIndex mDatos, mGestorLoc mLocalizaciones);
+int buscador(mGestorLoc mLocalizaciones, string nombre, int codigo);
 
 
 int main(){
@@ -109,6 +111,11 @@ void listadorDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, vector<tList
                 vLista[i].localizacion = mLocalizaciones[index].localizacion;
                 encontrado = true;
             }
+            else if(index == NLOC){
+                vLista[i].codigo = -1;
+                vLista[i].localizacion = to_string(vDemandado[i]);
+                encontrado = true;
+            }
             index++;
         }
         for(int j = 0; j < NCAT; j++){
@@ -120,6 +127,7 @@ void listadorDatos(mIndex mMigraciones, mGestorLoc mLocalizaciones, vector<tList
 }
 
 void imprimirDatos(vector<tListador>& vLista, int intro){
+    vector<int> vTemp;
     cout << "| " << setw(50) << left << "TERRITORIO" << "| " << setw(10) << "CODIGO" << "| " << setw(12) << 1990 << "| " << setw(12) << 1995  << "| " << setw(12) << 2000 << "| " << setw(12) << 2005 << "| " << setw(12) << 2010 << "| " << setw(12) << 2015 << "| " << setw(12) << 2020 << endl;
     for(int i = 0; i < 159; i++){
         if(i == 0){
@@ -131,42 +139,77 @@ void imprimirDatos(vector<tListador>& vLista, int intro){
     }
     cout << endl;
     for(int i = 0; i < vLista.size(); i++){
-        cout << "| " << setw(50) << vLista[i].localizacion << "| " << setw(10) << vLista[i].codigo; 
-        for(int j = 0; j < NANOS; j++){
-            if(intro == 1){
-                cout << "| " << setw(12) << vLista[i].poblTotal[j][0];
+        if(vLista[i].codigo != -1){
+            cout << "| " << setw(50) << vLista[i].localizacion << "| " << setw(10) << vLista[i].codigo; 
+            for(int j = 0; j < NANOS; j++){
+                if(intro == 1){
+                    cout << "| " << setw(12) << vLista[i].poblTotal[j][0];
+                }
+                if(intro == 2){
+                    cout << "| " << setw(12) << vLista[i].poblTotal[j][1];
+                }
+                if(intro == 3){
+                    cout << "| " << setw(12) << vLista[i].poblTotal[j][2];
+                }
             }
-            if(intro == 2){
-                cout << "| " << setw(12) << vLista[i].poblTotal[j][1];
-            }
-            if(intro == 3){
-                cout << "| " << setw(12) << vLista[i].poblTotal[j][2];
-            }
+        }
+        else{
+            vTemp.push_back(stoi(vLista[i].localizacion));
         }
         cout << endl;
     }
+    for(int i = 0; i < vTemp.size(); i++){
+        cout << "El codigo " << vTemp[i] << " no es coincidente con ninguna localizacion.";
+    }
 }
+
+int buscador(mGestorLoc mLocalizaciones, string nombre, int codigo){
+    bool encontrado = false;
+    string provNombre;
+    int index = 0;
+    for(int i = 0; i < nombre.size(); i++){
+        toupper(nombre[i]);
+    }
+    while(!encontrado){
+        provNombre = mLocalizaciones[index].localizacion;
+        for(int i = 0; i < provNombre.size(); i++){
+            toupper(provNombre[i]);
+        }
+        if(nombre == provNombre){
+            codigo = mLocalizaciones[index].codigo;
+            encontrado = true;
+        }
+        else if(index == NLOC){
+            encontrado = true;
+            codigo = -1;
+        }
+        index++;
+    }
+    return codigo;
+}
+
 
 void menu(vector<tListador> vLista, vector<int> vDemandado, mIndex mDatos, mGestorLoc mLocalizaciones){
     cout << "1. Listar todo" << endl;
     cout << "2. Buscar por codigo" << endl;
     cout << "3. Comparar localizaciones" << endl;
-    int opcion;
-    cin >> opcion;
-    system("cls");
+    cout << "4. Buscar por nombre" << endl;
 
     int codigo;
+    string localizacion;
+    char opcion = _getch();
+    system("cls");
 
     switch(opcion)
     {
-    case 1:
+    case '1':
         for(int i = 0; i < NLOC; i++){
             vDemandado.push_back(mDatos[i].lCodigo);
         }
         listadorDatos(mDatos, mLocalizaciones, vLista, vDemandado);
         imprimirDatos(vLista, 1);
         break;
-    case 2:
+    case '2':
         system("cls");
         cout << "Introducir el codigo de la localizacion a buscar: ";
         cin >> codigo;
@@ -174,7 +217,7 @@ void menu(vector<tListador> vLista, vector<int> vDemandado, mIndex mDatos, mGest
         listadorDatos(mDatos, mLocalizaciones, vLista, vDemandado);
         imprimirDatos(vLista, 1);
         break;
-    case 3:
+    case '3':
         system("cls");
         cout << "Introducir los codigos de las localizaciones a comparar: " << endl;
         cout << "- ";
@@ -184,9 +227,15 @@ void menu(vector<tListador> vLista, vector<int> vDemandado, mIndex mDatos, mGest
             cout << "- ";
             cin >> codigo;
         }
+        system("cls");
         listadorDatos(mDatos, mLocalizaciones, vLista, vDemandado);
         imprimirDatos(vLista, 1);
         break;
+    case '4':
+        system("cls");
+        cout << "Introducir el nombre de la localizacion a buscar: " << endl;
+        cin >> localizacion;
+
     default:
         break;
     }
